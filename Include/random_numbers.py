bdbfe517e62d231352735fb797766c673aa8d2fa
bruderooo <szymonjacon@gmail.com>
2020-04-29 19:09:03 +0200

    Neural gas works, but not ideal

--- Include/random_numbers.py
+++ Include/random_numbers.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def geometric_points(how_much_points, figure):
     i, x_axis, y_axis = 0, [], []
     while i < how_much_points:
-        tmp = 2 * np.random.rand(2) - 1
+        tmp = 2 * np.random.rand(2)
         if choose(tmp, figure):
             x_axis.append(tmp[0])
             y_axis.append(tmp[1])
@@ -15,22 +15,26 @@ def geometric_points(how_much_points, figure):
 
 def choose(list, type):
     if type == 'circle':
-        return (list[0] ** 2 + list[1] ** 2) <= 1
+        return ((list[0] - 1) ** 2 + (list[1] - 1) ** 2) <= 1
     elif type == 'rhombus':
-        return (abs(list[0]) + abs(list[1])) <= 1
+        return (abs(list[0] - 1) + abs(list[1] - 1)) <= 1
     elif type == 'funny_sq':
-        return (list[0] ** 4 + list[1] ** 4) <= 1
+        return ((list[0] - 1) ** 4 + (list[0] - 1) ** 4) <= 1
     elif type == 'star':
-        return (abs(list[0]) ** 0.5 + abs(list[1]) ** 0.5) <= 1
+        return (abs(list[0] - 1) ** 0.5 + abs(list[0] - 1) ** 0.5) <= 1
+    elif type == '4circle':
+        return (abs(list[0] - 1) - 0.5) ** 2 + (abs(list[1] - 1) - 0.5) ** 2 <= 0.09
+    elif type == 'small_circle':
+        return (list[0] ** 2 + list[1] ** 2) <= 0.1
 
 
-choose_list = ['circle', 'rhombus', 'funny_sq', 'star']
+choose_list = ['circle', 'rhombus', 'funny_sq', 'star', '4circle', 'small_circle']
 
-matrix = geometric_points(5, choose_list[1])
+matrix = geometric_points(100, choose_list[4])
 
 np.savetxt("training_set.txt", matrix.T)
 
-plt.axis(ymin=-1.25, ymax=1.25, xmin=-1.25, xmax=1.25)
+plt.axis(ymin=-0.25, ymax=2.25, xmin=-0.25, xmax=2.25)
 plt.grid(b=True)
 plt.plot(matrix[0], matrix[1], '.g')
 plt.gca().set_aspect('equal', 'box')
--- Include/self-organizing networks/neural_gas_one_winner.py
+++ Include/self-organizing networks/neural_gas_one_winner.py
@@ -19,8 +19,12 @@ def increse(number):
 
 # Distance function return euclidean distance
 def euclidean_distance(A_x, A_y, B_x, B_y):
-    # print(A_x, A_y, B_x, B_y)
     return ((A_x - B_x) ** 2 + (A_y - B_y) ** 2) ** 0.5
+
+
+# Distance in 2 axis
+def distance_xy(A_x, A_y, B_x, B_y):
+    return [abs(A_x - B_x), abs(A_y - B_y)]
 
 
 def draw(x_data, y_data, x_weights, y_weights):
@@ -31,7 +35,7 @@ def draw(x_data, y_data, x_weights, y_weights):
     plt.plot(x_data, y_data, '.g', x_weights, y_weights, '^r')
     plt.gca().set_aspect('equal', 'box')
 
-    plt.savefig(f"Plots/plot{increse(file_number)}.png")
+    plt.savefig(f"Plots_one_winner/plot{increse(file_number)}.png")
     plt.show()
 
     file_number += 1
@@ -41,7 +45,7 @@ if len(sys.argv) <= 1:
     # H = int(input("Podaj ilosc neuronow ukrytych: "))
     # epochs = int(input("Podaj ilość epok: "))
     # eta = float(input("Podaj krok: "))
-    H = 5
+    H = 50
     epochs = 1
     eta_k = 0.8 * np.random.rand(H)
 else:
@@ -64,24 +68,26 @@ weights = np.random.rand(N, H) * 1.5 + 0.25
 
 draw(data.T[0], data.T[1], weights[0], weights[1])
 
-for epoch in range(5):
+for epoch in range(10):
     for vector in enumerate(data):
         distance = np.asarray(euclidean_distance(vector[1][0], vector[1][1], weights[0], weights[1]))
         sorted_distance = np.sort(distance.copy())
 
-        for neuron in enumerate(sorted_distance):
-            # i[0][0] - is index od neuron
-            i = np.where(distance == sorted_distance[0])[0][0]
+        # i[0][0] - is index od neuron
+        i = np.where(distance == sorted_distance[0])[0][0]
 
-            # Obliczanie eta dla danej iteracji
-            eta_k[i] = eta_max * (eta_min / eta_max) ** (vector[0] / M)
+        # Obliczanie eta dla danej iteracji
+        eta_k[i] = eta_max * (eta_min / eta_max) ** (vector[0] / M)
 
-            # Obliczanie lambdy dla danej iteracji
-            lambd = (H / 2) * (lambd_min / (H / 2)) ** (vector[0] / M)
+        # print("distance:", distance)
+        # print("sorted_distance:", sorted_distance)
+        # print("Dane:", vector[1])
+        # print("Indeks wygranego neuronu:", i)
+        # print("Neuron wygrany", weights.T[i])
+        # print("To dodamy do wagi ^:", eta_k[i] * (distance_2D.T[i] - weights.T[i]))
 
-            # Obliczanie wagi dla następnej iteracji
-            # weights.T[i] += eta_k[i] * np.exp(-neuron[0] / lambd) * (vector[1] - weights.T[i])
+        # input()
 
-            weights.T[i] += eta_k[i] * (vector[1] - weights.T[i])
+        weights.T[i] += eta_k[i] * (vector[1] - weights.T[i])
 
         draw(data.T[0], data.T[1], weights[0], weights[1])
