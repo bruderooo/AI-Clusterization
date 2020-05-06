9e257bdff1e84b7c25ea7449c58993e741526759
bruderooo <szymonjacon@gmail.com>
2020-05-06 18:36:56 +0200

    Neural gas works briliant, k-means clustering works good and kohonen works not bad, but also not good

--- Include/k-means clustering/kmeans_clustering.py
+++ Include/k-means clustering/kmeans_clustering.py
@@ -1,45 +1,75 @@
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib import pyplot as plt
 import numpy as np
+from Include import euclidean_distance, increse, make_gif, prepare_data
+
+file_number = 1
 
 
-# Distance function return euclidean distance
-def euclidean_distance(A_x, A_y, B_x, B_y):
-    return ((A_x - B_x) ** 2 + (A_y - B_y) ** 2) ** 0.5
+def draw(x_data, y_data, weights):
+    global file_number
+
+    plt.cla()
+    voronoi_plot_2d(weights, show_vertices=False, line_colors='red', line_width=2, line_alpha=1, point_size=20)
+    # plt.axis(ymin=-0.25, ymax=2.25, xmin=-0.25, xmax=2.25)
+    plt.axis(ymin=-10, ymax=10, xmin=-10, xmax=10)
+    plt.grid(b=True)
+    plt.plot(x_data, y_data, '.g')
+    plt.gca().set_aspect('equal', 'box')
+    plt.savefig(f"Plots/plot{increse(file_number)}.png")
+    plt.show(block=False)
+    file_number += 1
 
 
-k = 3
+def how_much_var_in_array(array, var):
+    n = 0
+    for i in array:
+        if i[2] == var:
+            n += 1
+    return n
+
+
+k = int(input("Choose number of clusters: "))
 
 clusters = np.random.rand(k, 2) + 0.5
 
 vor = Voronoi(clusters)
 
-# lista = []
-# for i in range(1, 10_000, 20):
-#     lista.append(i)
-#
-# lista = tuple(lista)
-# data = np.loadtxt("../test.txt", delimiter=',', usecols=lista)
+# Load training set
+if "1" == input("Choose data set. '1' - for random points, other for wikamp test data: "):
+    data = np.loadtxt("../training_set.txt")
+else:
+    prepare_data("../test.txt", "../test_prepared.txt", ",", 5)
+    data = np.loadtxt("../test_prepared.txt")
 
-data = np.loadtxt("../training_set.txt")
 M = len(data)
 data = np.concatenate((data, np.zeros((M, 1))), axis=1)
 
-voronoi_plot_2d(vor, show_vertices=False, line_colors='red', line_width=2, line_alpha=1, point_size=20)
+draw(data.T[0], data.T[1], vor)
 
-plt.axis(ymin=-0.25, ymax=2.25, xmin=-0.25, xmax=2.25)
-plt.grid(b=True)
-plt.gca().set_aspect('equal', 'box')
-plt.plot(data.T[0], data.T[1], '.g')
-plt.show()
+for epoch in range(10):
+    tmp_clusters = np.zeros((k, 2))
 
-for vector in enumerate(data):
-    distance = np.asarray(euclidean_distance(vector[1][0], vector[1][1], clusters.T[0], clusters.T[1]))
-    sorted_distance = np.sort(distance.copy())
+    for vector in enumerate(data):
+        distance = np.asarray(euclidean_distance(vector[1][0], vector[1][1], clusters.T[0], clusters.T[1]))
+        sorted_distance = np.sort(distance.copy())
 
-    # Index "która grupa"
-    i = np.where(distance == sorted_distance[0])[0][0]
+        # Index "która grupa"
+        i = np.where(distance == sorted_distance[0])[0][0]
 
-    vector[1][2] = i
+        vector[1][2] = i
 
-print(data)
+        tmp_clusters[i][0] += vector[1][0]
+        tmp_clusters[i][1] += vector[1][1]
+
+    for i in range(k):
+        tmp = how_much_var_in_array(data, i)
+        if 0 != tmp:
+            clusters[i][0] = tmp_clusters[i][0] / tmp
+            clusters[i][1] = tmp_clusters[i][1] / tmp
+
+    vor = Voronoi(clusters)
+
+    draw(data.T[0], data.T[1], vor)
+
+make_gif("./Plots/*", "clustry")
--- Include/self-organizing networks/self-organizing_networks.py
+++ Include/self-organizing networks/self-organizing_networks.py
@@ -2,39 +2,20 @@ from matplotlib import pyplot as plt
 import numpy as np
 import sys
 
+from Include import prepare_data
+from Include import increse, euclidean_distance, make_gif
 
 file_number = 1
-
-
-def increse(number):
-    number = str(number)
-    if len(number) == 4:
-        return number
-    elif len(number) == 3:
-        return "0" + number
-    elif len(number) == 2:
-        return "00" + number
-    elif len(number) == 1:
-        return "000" + number
-    elif len(number) == 5:
-        return "0000" + number
-
-
-# Distance function return euclidean distance
-def euclidean_distance(A_x, A_y, B_x, B_y):
-    return ((A_x - B_x) ** 2 + (A_y - B_y) ** 2) ** 0.5
 
 
 def draw(x_data, y_data, x_weights, y_weights):
     global file_number
 
     plt.cla()
-    plt.axis(ymin=-0.25, ymax=2.25, xmin=-0.25, xmax=2.25)
     plt.grid(b=True)
     plt.plot(x_data, y_data, '.g', x_weights, y_weights, '^r')
     plt.gca().set_aspect('equal', 'box')
     plt.savefig(f"Plots/plot{increse(file_number)}.png")
-    # plt.draw()
     plt.show(block=False)
     file_number += 1
 
@@ -49,8 +30,15 @@ else:
 if algorithm == "kohonen":
     R = float(input("Podaj promien sasiedztwa: "))
 
+
 # Load training set
-data = np.loadtxt("../training_set.txt")
+if "1" == input("Choose data set. '1' - for random points, other for wikamp test data: "):
+    data = np.loadtxt("../training_set.txt")
+else:
+    prepare_data("../test.txt", "../test_prepared.txt", ",", 5)
+    data = np.loadtxt("../test_prepared.txt")
+
+
 M = len(data)
 
 # Number of dimension
@@ -60,31 +48,35 @@ eta_max = 0.8
 eta_min = 0.003
 lambd_min = 0.01
 
-weights = np.random.rand(N, H) * 2
+weights = np.random.rand(N, H) * 15 - 7.5
 # plt.xkcd()
 draw(data.T[0], data.T[1], weights[0], weights[1])
 
-for vector in enumerate(data):
+for epoch in range(10):
+    for vector in enumerate(data):
 
-    distance = np.asarray(euclidean_distance(vector[1][0], vector[1][1], weights[0], weights[1]))
-    sorted_distance = np.sort(distance.copy())
+        distance = np.asarray(euclidean_distance(vector[1][0], vector[1][1], weights[0], weights[1]))
+        sorted_distance = np.sort(distance.copy())
 
-    for neuron in enumerate(sorted_distance):
-        # i[0][0] - is index od neuron
-        i = np.where(distance == neuron[1])[0][0]
+        for neuron in enumerate(sorted_distance):
+            # i[0][0] - is index od neuron
+            i = np.where(distance == neuron[1])[0][0]
 
-        # Obliczanie eta dla danej iteracji
-        eta_k = eta_max * (eta_min / eta_max) ** (vector[0] / M)
+            # Obliczanie eta dla danej iteracji
+            eta_k = eta_max * (eta_min / eta_max) ** (vector[0] / M)
 
-        if algorithm == "gas":
-            # Obliczanie lambdy dla danej iteracji
-            lambd = (H / 2.0) * (lambd_min / (H / 2.0)) ** (vector[0] / M)
+            if algorithm == "gas":
+                # Obliczanie lambdy dla danej iteracji
+                lambd = (H / 2.0) * (lambd_min / (H / 2.0)) ** (vector[0] / M)
 
-            weights.T[i] += eta_k * np.exp(-neuron[0] / lambd) * (vector[1] - weights.T[i])
+                weights.T[i] += eta_k * np.exp(-neuron[0] / lambd) * (vector[1] - weights.T[i])
 
-        elif algorithm == "kohonen":
-            winner_index = np.where(distance == sorted_distance[0])[0][0]
-            dist = euclidean_distance(weights.T[winner_index][1], weights.T[winner_index][1], weights.T[i][1], weights.T[i][1])
-            weights.T[i] += eta_k * np.exp(-(dist ** 2 / (2 * R ** 2))) * (vector[1] - weights.T[i])
+            elif algorithm == "kohonen":
+                winner_index = np.where(distance == sorted_distance[0])[0][0]
+                dist = euclidean_distance(weights.T[winner_index][1], weights.T[winner_index][1], weights.T[i][1], weights.T[i][1])
+                weights.T[i] += eta_k * np.exp(-(dist ** 2 / (2 * R ** 2))) * (vector[1] - weights.T[i])
 
-    draw(data.T[0], data.T[1], weights[0], weights[1])
+        if (vector[0] % (M / 2)) == 0:
+            draw(data.T[0], data.T[1], weights[0], weights[1])
+
+make_gif("./Plots/*", algorithm)
